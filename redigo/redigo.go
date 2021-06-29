0625afbe8208d90f99225741636e5246f37ce107
gordondu <gordondu@tencent.com>
2021-06-29 19:04:01 +0800

    modify test

--- redigo/redigo.go
+++ redigo/redigo.go
@@ -18,47 +18,34 @@ func main() {
 	fmt.Printf("Start redigo test on %s:%d", *flHost, *flPort)
 
 	pool := &redis.Pool{
-		MaxIdle:   8000,
-		MaxActive: 10000,
-		// Dial or DialContext must be set. When both are set, DialContext takes precedence over Dial.
+		MaxIdle:   4000,
+		MaxActive: 5000,
 		Dial: func() (redis.Conn, error) {
-			return redis.Dial("tcp", fmt.Sprintf("%s:%d", *flHost, *flPort),
+			c, connErr := redis.Dial("tcp", fmt.Sprintf("%s:%d", *flHost, *flPort),
 				redis.DialConnectTimeout(time.Duration(1000)*time.Millisecond),
-				redis.DialReadTimeout(time.Duration(1000)*time.Millisecond),
-				redis.DialWriteTimeout(time.Duration(1000)*time.Millisecond))
+				redis.DialReadTimeout(time.Duration(500)*time.Millisecond),
+				redis.DialWriteTimeout(time.Duration(500)*time.Millisecond))
+			if connErr != nil {
+				return nil, connErr
+			}
+
+			_, authErr := c.Do("auth", *flAuth)
+			if authErr != nil {
+				c.Close()
+				return nil, authErr
+			}
+			return c, nil
 		},
 		Lifo: false,
 		Wait: true,
 	}
 	defer pool.Close()
-	for i := 0; i < 5000000; i++ {
+	for i := 0; i < 1000000; i++ {
 		go func() {
 			for {
 				conn := pool.Get()
-				_, err := conn.Do("auth", *flAuth)
-				if err != nil {
-					//	fmt.Printf("err when auth! err: %v\n", err)
-					//	os.Exit(0)
-				}
-
-				_, err = conn.Do("ping")
-				if err != nil {
-					//	fmt.Printf("err when ping! err: %v\n", err)
-					//	os.Exit(0)
-				}
-
-				_, err = conn.Do("hset", rand.Int31n(100), rand.Int31n(1000),
-					fmt.Sprintf("%d:%d:%d:%d:%d:%d:%d:%d:%d:%d", rand.Int31(), rand.Int31(), rand.Int31(), rand.Int31(), rand.Int31(), rand.Int31(), rand.Int31(), rand.Int31(), rand.Int31(), rand.Int31()))
-				if err != nil {
-					//	fmt.Printf("err when hset! err: %v\n", err)
-					//os.Exit(0)
-				}
-
-				_, err = conn.Do("hgetall", rand.Int31n(100))
-				if err != nil {
-					//	fmt.Printf("err when hset! err: %v\n", err)
-					//os.Exit(0)
-				}
+				_, _ = conn.Do("hset", rand.Int31n(100), rand.Int31n(1000), rand.Int31())
+				time.Sleep(time.Duration(rand.Int63n(5)) * time.Millisecond)
 				conn.Close()
 			}
 		}()
